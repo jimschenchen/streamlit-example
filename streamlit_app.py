1231304663cd4a3d11128dd77d9dbb93fe4026dd
chenyue_li <chenyue.li@huawei.com>
2022-11-09 11:37:45 -0500

    add

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,26 +112,25 @@ plt.legend()
 
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    # total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+    # num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    # Point = namedtuple('Point', 'x y')
+    # data = []
 
-    points_per_turn = total_points / num_turns
+    # points_per_turn = total_points / num_turns
+    df = pd.DataFrame({"real_stock_price": real_stock_price.flatten(),
+                  "predicted_stock_price": predicted_stock_price.flatten()})
+    st.line_chart(df)
 
-    st.line_chart(pd.DataFrame({"real_stock_price": real_stock_price,
-                  "predicted_stock_price": predicted_stock_price}))
-    st.line_chart()
+    # for curr_point_num in range(total_points):
+    #     curr_turn, i = divmod(curr_point_num, points_per_turn)
+    #     angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+    #     radius = curr_point_num / total_points
+    #     x = radius * math.cos(angle)
+    #     y = radius * math.sin(angle)
+    #     data.append(Point(x, y))
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-                    .mark_circle(color='#0068c9', opacity=0.5)
-                    .encode(x='x:Q', y='y:Q'))
+    # st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+    #                 .mark_circle(color='#0068c9', opacity=0.5)
+    #                 .encode(x='x:Q', y='y:Q'))
